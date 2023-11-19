8358973a1b1239d38bfd522270bf02068d2aacca
Rottab <totundberuhmt@gmail.com>
2023-11-19 18:46:07 +0200

    Boiler Plate for REact-Redux.

---
+++ src/reducers/postReducer.js
@@ -0,0 +1,23 @@
+import { FETCH_POSTS, NEW_POSTS } from "../actions/types";
+
+const intialState = {
+  items: [],
+  item: {},
+};
+
+export default function (state = intialState, action) {
+  switch (action.type) {
+    case FETCH_POSTS:
+      return {
+        ...state,
+        items: action.payload,
+      };
+    case NEW_POSTS:
+      return {
+        ...state,
+        item: action.payload,
+      };
+    default:
+      return state;
+  }
+}
